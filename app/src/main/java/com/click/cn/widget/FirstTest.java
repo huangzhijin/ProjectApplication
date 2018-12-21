d041a30bc405018a11cd5c893aa12387aa81cb7c
13614 <1361497021@qq.com>
2018-12-21 16:48:44 +0800

    branch1--first

--- app/src/main/java/com/click/cn/widget/FirstTest.java
+++ app/src/main/java/com/click/cn/widget/FirstTest.java
@@ -3,4 +3,6 @@ package com.click.cn.widget;
 public class FirstTest {
     //----------------------master----------------------
 
+    //ceshi cherry-pick------------branch1
+    //----------1----------------branch1--------------------
 }
